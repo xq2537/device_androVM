90021720a27fe08868caf19465bef9ab379c0293
Daniel Fages <dan@androvm.org>
2012-12-03 23:39:49 +0100

    - Remove testing targets

--- vbox86t_atom/BoardConfig.mk
+++
@@ -1,4 +0,0 @@
-# Use common part from vbox86
-
-include device/androVM/vbox86/BoardConfig.mk
-TARGET_ARCH_VARIANT := x86-atom
--- vbox86t_atom/device.mk
+++
@@ -1,5 +0,0 @@
-PRODUCT_CHARACTERISTICS := tablet
-
-DEVICE_PACKAGE_OVERLAYS := device/androVM/vbox86t_atom/overlay
-
-$(call inherit-product, device/androVM/vbox86/device.mk)
